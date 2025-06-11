3e5567d5edc5ea25b591c19c395af803c021cad4
Abdullah Al Mahmud <abdullahmahmud01798@gmail.com>
2025-06-11 12:17:55 +0600

    oop

--- Practice.cpp
+++ Practice.cpp
@@ -1,11 +1,35 @@
 #include<iostream>
 using namespace std;
 
-class  {
+class Rectangle {
+    int length , width;
+    public:
+        Rectangle(){
+            length = 1;
+            width = 1;
+        }
 
+        Rectangle(int length , int width){
+            this->length = length;
+            this->width = width;
+        }
+
+        int area(){
+            return length * width;
+        }
+
+        void display(){
+            cout << "Length : " << length << "\n";
+            cout << "Width : " << width << "\n";
+            cout << "\n";
+            cout << "Area : " << area() << "\n";
+            cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+        }
 };
 
 int main() {
-    
-   return 0;
+    Rectangle r1 , r2(10 , 5);
+    r1.display();
+    r2.display();
+    return 0;
 }
