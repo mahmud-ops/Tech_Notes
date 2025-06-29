3d4a9dd94ecfa1522596c749d7205f66fd94af43
Abdullah Al Mahmud <abdullahmahmud01798@gmail.com>
2025-06-29 18:45:32 +0600

    Removed .exe files

--- Practice.cpp
+++ Practice.cpp
@@ -1,19 +1,20 @@
-#include<iostream>
-#include<stack>
-
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-    stack<int> mystack;
-    
-    mystack.push(1);
-    mystack.push(2);
-    mystack.push(3);
-    mystack.push(4);
 
-    while(!mystack.empty()){
-        cout << mystack.top() << " ";
-        mystack.pop();
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        auto it_1 = nums.begin();
+        vector<int> :: iterator it_2; 
+        for(it_1; it_1 < nums.end() ; it_1++){
+            it_2 = find(nums.begin(),nums.end(),target);
+        }
+        if(it_1 != nums.end()){
+            result.push_back(it_1 - nums.begin());
+            result.push_back(it_2 - nums.begin());
+        }
+
+        return result;
     }
-
-    return 0;
-}
+};
